b142ead06763f1fefd489e84862faf9ddf5fb9fc
mehmetCt <mehmet@cybertekschool.com>
2023-11-22 11:07:25 -0500

    Library APP TEMPLATE for DB Test

--- src/test/resources/queries/practice.sql
+++ src/test/resources/queries/practice.sql
@@ -7,3 +7,9 @@ FROM books B
     select  count(id) from users; -- 4891
 
     select count(distinct id) from users; -- 4891
+
+
+select bc.name,count(*) from book_borrow bb
+                                 inner join books b on bb.book_id = b.id
+                                 inner join book_categories bc on b.book_category_id=bc.id
+group by name order by 2 desc;
