d161005f7263d8806bb21770358f9b36299c8c76
mehmetCt <mehmet@cybertekschool.com>
2023-11-22 12:52:00 -0500

    US03

--- src/test/java/com/library/steps/BookStepDefs.java
+++ src/test/java/com/library/steps/BookStepDefs.java
@@ -2,8 +2,10 @@ package com.library.steps;
 
 import com.library.pages.BookPage;
 import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 import java.util.List;
 
@@ -18,10 +20,10 @@ public class BookStepDefs  {
 
 
     }
-
+    List<String> actualCategoryList;
     @When("the user clicks book categories")
     public void the_user_clicks_book_categories() {
-        List<String> actualCategoryList = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        actualCategoryList = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
         System.out.println("actualCategoryList = " + actualCategoryList);
 
         // Remove ALL from list
@@ -33,6 +35,13 @@ public class BookStepDefs  {
 
     @Then("verify book categories must match book_categories table from db")
     public void verify_book_categories_must_match_book_categories_table_from_db() {
+
+        DB_Util.runQuery("select name from book_categories");
+
+        List<String> expectedCategoryList = DB_Util.getColumnDataAsList(1);
+        System.out.println("expectedCategoryList = " + expectedCategoryList);
+
+        Assert.assertEquals(expectedCategoryList,actualCategoryList);
 
 
     }
