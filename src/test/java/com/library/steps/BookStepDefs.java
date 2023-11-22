71cf8b83c5058fc22e77266f73405999d167a75a
mehmetCt <mehmet@cybertekschool.com>
2023-11-22 12:43:43 -0500

    US03

--- src/test/java/com/library/steps/BookStepDefs.java
+++ src/test/java/com/library/steps/BookStepDefs.java
@@ -1,17 +1,33 @@
 package com.library.steps;
 
+import com.library.pages.BookPage;
+import com.library.utility.BrowserUtil;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class BookStepDefs {
+import java.util.List;
+
+public class BookStepDefs  {
+
+    BookPage bookPage=new BookPage();
 
     @When("the user navigates to {string} page")
     public void the_user_navigates_to_page(String moduleName) {
+        bookPage.navigateModule(moduleName);
+        BrowserUtil.waitFor(2);
+
 
     }
 
     @When("the user clicks book categories")
     public void the_user_clicks_book_categories() {
+        List<String> actualCategoryList = BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        System.out.println("actualCategoryList = " + actualCategoryList);
+
+        // Remove ALL from list
+        actualCategoryList.remove(0);
+        System.out.println(" After remove ALL Option");
+        System.out.println("actualCategoryList = " + actualCategoryList);
 
     }
 
